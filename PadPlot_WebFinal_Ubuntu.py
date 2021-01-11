43f97a804b74ea1e8fcfe976c4659744fb48cb74
Simon Fisher <60694318+SimonF92@users.noreply.github.com>
2021-01-11 14:44:21 +0000

    Update PadPlot_WebFinal_Ubuntu.py

--- PadPlot_WebFinal_Ubuntu.py
+++ PadPlot_WebFinal_Ubuntu.py
@@ -645,7 +645,7 @@ def geneset_enrichment():
                         outdir=None,  # do not write output to disk
                         no_plot=True, # Skip plotting
                         method=statmethod, # or t_test
-                         seed= 7,
+                        processes=0,seed= 7,
                         format='png')
 
         return(gs_res)
